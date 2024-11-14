7549249374b62932d5b591f3ba49ad00d0ae6082
suhyeon1103 <tngus74501@gmail.com>
2024-11-15 00:36:28 +0900

    Acc parctice

---
+++ dist/practice.js
@@ -0,0 +1,16 @@
+"use strict";
+const message = "Hello TypeScript!";
+console.log(message);
+const number = 29;
+console.log(number);
+const isDone = false;
+console.log(isDone);
+const Null = null;
+console.log(Null);
+let anything;
+anything = 29;
+console.log(anything);
+anything = "Anything else?";
+console.log(anything);
+anything = ["a", "b", "c"];
+console.log(anything);
---
+++ practice.ts
@@ -0,0 +1,26 @@
+const message: string = "Hello TypeScript!";
+
+console.log(message);
+
+const number: number = 29;
+
+console.log(number);
+
+const isDone: boolean = false;
+
+console.log(isDone);
+
+const Null: null = null;
+
+console.log(Null);
+
+let anything: any;
+
+anything = 29;
+console.log(anything);
+
+anything = "Anything else?";
+console.log(anything);
+
+anything = ["a", "b", "c"];
+console.log(anything);
